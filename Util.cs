4ce4584a16ffa2fc0b65da62d29a7772aede7ca2
dahdo <dndashimye15@gmail.com>
2023-05-16 16:26:29 +0200

    addition of utils

---
+++ Util.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BajtpikOOD {
+    internal class Util {
+        public static List<string> GetFields(Type type) {
+            PropertyInfo[] properties = type.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            List<string> fields = new List<string>();
+            foreach (var prop in properties) {
+                if (prop.Name == "Authors") {
+                    continue;
+                }
+                fields.Add(prop.Name);
+            }
+            return fields;
+        }
+    }
+}
