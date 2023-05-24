84e1c497dc8408f732f348f37f7166bcc2b195b8
dahdo <dndashimye15@gmail.com>
2023-05-24 23:51:26 +0200

    added more functions

--- Util.cs
+++ Util.cs
@@ -18,5 +18,48 @@ namespace BajtpikOOD {
             }
             return fields;
         }
+
+        public static List<string> SecondaryLoop(List<string> fields) {
+            Console.Write("Available fields: [  ");
+            foreach (string field in fields) {
+                Console.Write($"{field}  ");
+            }
+            Console.WriteLine("]");
+
+            List<string> inputList = new List<string>();
+            string? input = Console.ReadLine();
+            while (input?.ToLower() != "exit" && input?.ToLower() != "done") {
+                if(input != null)
+                    inputList.Add(input);               
+                input = Console.ReadLine();
+            }
+
+            return inputList;
+        }
+
+        public static List<string> GetFieldVal(string input) {
+            var vals = new List<string>();
+            for (int i = 0; i < input.Length; i++) {
+                if (input[i] == '=') {
+                    vals.Add(input.Substring(0, i));
+
+                    int startIdx = i + 1;
+
+                    if (startIdx < input.Length && input[startIdx] == '\"') {
+                        int endIdx = input.IndexOf('\"', startIdx + 1);
+                        if (endIdx != -1) {
+                            vals.Add(input.Substring(startIdx + 1, endIdx - startIdx - 1));
+                            break;
+                        }
+                    }
+                    else {
+                        vals.Add(input.Substring(startIdx, input.Length - startIdx));
+                        break;
+                    }
+                }
+            }
+            return vals;
+        }
     }
+
 }
