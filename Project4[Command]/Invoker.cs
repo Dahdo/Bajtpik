e7166311203a0828aa37a85207efa6bbc2758ef2
dahdo <dndashimye15@gmail.com>
2023-05-24 19:15:15 +0200

    implement invoker

--- Project4[Command]/Invoker.cs
+++ Project4[Command]/Invoker.cs
@@ -4,7 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BajtpikOOD.Project4_Command_ {
-    internal class Invoker {
+namespace Project4_Command {
+    public class Invoker {
+        private static Queue<ICommand>? CommandQueue;
+        private Invoker() {}
+
+        public static void AddCommand(ICommand command) {
+            if(CommandQueue == null) {
+                Invoker.CommandQueue = new Queue<ICommand>();
+            }
+            CommandQueue.Enqueue(command);
+        }
+
+        public static void Print() {
+            if (Invoker.CommandQueue == null)
+                return;
+            foreach(var command in CommandQueue) {
+                Console.WriteLine(command.ToString());
+            }
+        }
+
+        public static void Export() {
+            new NotImplementedException();
+        }
+
+        public static void Commit() {
+            if (Invoker.CommandQueue == null) 
+                return;
+            foreach(var command in CommandQueue) {
+                command.Execute();
+            }
+        }
     }
 }
